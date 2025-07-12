72f3e10da310300ae7a0675406cdc7b99243ab24
Matheus <matheusmartinscia@gmail.com>
2025-07-12 15:42:49 -0300

    Feat: Importa inline components

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,16 @@
 <x-layouts.main_layout>
     <x-slot:content>
-        
+
         <h1 class="display-6 text-center">LIVEWIRE</h1>
         <hr>
 
         <livewire:counter />
+
+        @php
+            $dynamic = "DYNA";    
+        @endphp
+
+        <h1 class="mt-5">INLINE</h1>
+        <livewire:inline-component value="Matth" :dynamicValue="$dynamic"/>
     </x-slot:content>
 </x-layouts.main_layout>
