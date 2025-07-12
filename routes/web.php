97aa69261908f21b2f813bdfe5a729639905addd
Matheus <matheusmartinscia@gmail.com>
2025-07-12 16:58:15 -0300

    Feat: Adiciona rota para view 'Client'

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,6 @@ use App\Livewire\Counter;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [MainController::class, 'home'])->name('home');
+Route::get('/clients', [MainController::class, 'clients'])->name('clients');
 
 /* Route::get('/counter', Counter::class); */
