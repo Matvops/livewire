cdd3d37020400c90f1e6a61359cad50ca9594fbd
Matheus <matheusmartinscia@gmail.com>
2025-07-12 16:57:17 -0300

    Feat: Cria método clients

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -12,4 +12,37 @@ class MainController extends Controller
     {
         return view('home', ['dynamicController' => "VALOR DINAMICO"]);
     }
+
+    public function clients():View
+    {
+        $clients = [
+            [
+                'id' => 1,
+                'name' => "Client 1",
+                'phone' => 123456781
+            ],
+            [
+                'id' => 2,
+                'name' => "Client 2",
+                'phone' => 123456782
+            ],
+            [
+                'id' => 3,
+                'name' => "Client 3",
+                'phone' => 123456783
+            ],
+            [
+                'id' => 4,
+                'name' => "Client 4",
+                'phone' => 123456784
+            ],
+            [
+                'id' => 5,
+                'name' => "Client 5",
+                'phone' => 123456785
+            ],
+        ];
+
+        return view('client', ['clients' => $clients]);
+    }
 }
