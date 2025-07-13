ec322f340e017ab96a461fe4be3ef2406a153ce0
Matheus <matheusmartinscia@gmail.com>
2025-07-13 12:45:13 -0300

    Feat: Cria rotas para component full page

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,13 @@
 
 use App\Http\Controllers\MainController;
 use App\Livewire\Counter;
+use App\Livewire\FullPage;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [MainController::class, 'home'])->name('home');
 Route::get('/clients', [MainController::class, 'clients'])->name('clients');
 Route::view('/form', 'form');
+Route::get('/full-page', FullPage::class);
 
 
 /* Route::get('/counter', Counter::class); */
